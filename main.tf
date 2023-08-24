4f57b23e00cc6e71453946532473a1cfb75cd4c1
rasoolasn <rasoolasn@gmail.com>
2023-08-24 16:00:55 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,11 +1,8 @@
 
 module "components" {
-  source          = "git::https://github.com/reach2nabi/tf-module-basic-test.git"
+    source          = "git::https://github.com/reach2nabi/tf-module-vpc.git"
 
-  for_each        = var.components
-  security_group  = var.security_group
-  zone_id         = var.zone_id
-  instance_type   = each.value["instance_type"]
-  name            = each.value["name"]
+    for_each        = var.vpc
+    cidr            = var["cidr"]
 
 }
--- vars.tf
+++ vars.tf
@@ -1,3 +1,2 @@
-variable "components" {}
-variable "security_group" {}
-variable "zone_id" {}
+variable "vpc" {}
+
